3e16fda2eab71c814c864ee3701e78a0de08eb92
trilokee <trilokeekumargupta@gmail.com>
2023-02-09 23:36:56 +0530

    saveProducts api added

--- readme.md
+++ readme.md
@@ -3,3 +3,5 @@
 <!-- npm i express (for installing node module)--> 
 
 <!-- npm i nodemon -D (for automatic changes detection like dev tool in sts)--> 
+<!-- npm run dev(for running app using nodemon)--> 
+<!-- npm i mongoose(for installing mongo db)--> 
--- server.js
+++ server.js
@@ -1,8 +1,51 @@
 var express = require('express')
+const mongoose = require('mongoose')
+var mysql = require('mysql');
+const Product = require('./models/productModels')
 var app = express()
-console.log('ok')
+app.use(express.json())//middleware 
+app.use(express.urlencoded({extended: false}))
 app.get('/', function (req, res) {
   res.send('hello world')
 })  
 
-app.listen(3000)
+app.get('/trilokee', function (req, res) {
+  res.send('hello trilokee')
+})  
+
+app.get('/getData', function (req, res) {
+  res.send('hello data')
+}) 
+
+app.post('/saveProducts', async(req, res) => {
+  try {
+      const product = await Product.create(req.body)
+      res.status(200).json(product);
+      
+  } catch (error) {
+      console.log(error.message);
+      res.status(500).json({message: error.message})
+  }
+})
+mongoose.set('strictQuery', false);
+//mysql connection 
+// var con = mysql.createConnection({
+//   host: "localhost",
+//   user: "root",
+//   password: "@Tn33333"
+// });
+
+// con.connect(function(err) {
+//   if (err) throw err;
+//   console.log("Connected! to mysql");
+//   app.listen(3000,()=>{
+//     console.log('NODE_CRID_API RUNNING ON PORT 3000');
+//   })
+// });
+//mongo connection 
+mongoose.connect('mongodb://localhost:27017/Node-CRUD-API?retryWrites=true&w=majority').then(()=>{
+  app.listen(3000,()=>{
+    console.log('NODE_CRID_API RUNNING ON PORT 3000');
+  })
+  console.log('connected to mongodb');
+}).catch((error)=>{console.log('Error while connecting mongodb',error);});
